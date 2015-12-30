68b3523cf60cef38a72e5664aedb1e9b67d947dd
mouse <changyou.gong@gumichina.com>
2015-12-30 19:00:49 +0800

    lua test init

---
+++ Assets/Editor/AssetBundleTool.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+public class AssetBundleTool
+{
+#if true
+	// コマンド
+	[MenuItem("Assets/Export Asset Bundle")]
+	static void ExportSelection(){
+
+		var assets = Selection.GetFiltered(typeof(Object), SelectionMode.DeepAssets);//Assets/path/to/file
+		var assetObjs = new List<Object> ();
+		var explicitAssetNames = new List<string> ();
+		foreach (var asset in assets) {
+			var path = AssetDatabase.GetAssetPath(asset);
+
+			var ext = System.IO.Path.GetExtension(path);
+//			var exportName = path.Replace("/", "_").Replace(".", "_");
+			var exportName = Path.GetFileName (path);
+			exportName = exportName.Substring (0, exportName.IndexOf ("."));
+			Debug.LogError ("Export Name:" + exportName);
+			if(ext == ".meta")
+				continue;
+			try{
+				var assetObj = AssetDatabase.LoadAssetAtPath(path, typeof(Object));
+				assetObjs.Add(assetObj);
+				explicitAssetNames.Add(exportName);
+			}catch{
+				Debug.LogError("load asset " + path + " fail");
+			}	
+		}
+
+		var exportPath = Path.Combine( LuaScriptsFromFile.luaAssetsPath, "lua.unity3d");
+		
+		BuildPipeline.PushAssetDependencies();
+		BuildPipeline.BuildAssetBundleExplicitAssetNames(assetObjs.ToArray(), explicitAssetNames.ToArray(), exportPath,
+		                                                 BuildAssetBundleOptions.CollectDependencies | BuildAssetBundleOptions.CompleteAssets, BuildTarget.iPhone);
+		BuildPipeline.PopAssetDependencies();
+
+
+//		AssetDatabase.Refresh();//这个要很久
+		// AssetBundleが更新されたらローカルキャッシュをクリアする
+		Caching.CleanCache();
+
+		
+//			string sourceVersionRoot = App.Path.ExternalResources + "_Version/" + NetworkManager.PLATFORM_SYMBOL;
+//			string targetVersionRoot = App.Path.ProjectRoot + "Assets/App/Resources/_Version/" + NetworkManager.PLATFORM_SYMBOL;
+//			FileUtil.DeleteFileOrDirectory(targetVersionRoot);
+//			FileUtil.CopyFileOrDirectory(sourceVersionRoot, targetVersionRoot);
+
+	}
+
+	
+	// ファイルを検索
+	public static List<string> SearchFiles(string path, bool recursive = true)
+	{
+		List<string> ret = new List<string>();
+		try
+		{
+			foreach(string file in System.IO.Directory.GetFiles(path))
+			{
+				ret.Add(file);
+			}
+			if (recursive)
+			{
+				foreach(string dir in System.IO.Directory.GetDirectories(path))
+				{
+					ret.AddRange(SearchFiles(dir, recursive));
+				}
+			}
+		}
+		catch
+		{
+		}
+		return ret;
+	}
+
+
+#endif
+}
---
+++ Assets/Lua/LuaScriptsFromFile.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using LuaInterface;
+using System.IO;
+public class LuaScriptsFromFile : MonoBehaviour {
+	public static string luaAssetsPath = Application.streamingAssetsPath + "/LuaAssets/";
+    //public TextAsset scriptFile;
+
+	// Use this for initialization
+	void Start () {
+
+		StartCoroutine (AsyncScript ("lua.unity3d", delegate(AssetBundle luaAsset) {
+			//TextAsset scriptText = obj.Load ("04_ScriptsFromFile.lua.txt") as TextAsset;
+			Debug.LogError ("luaState: " + (3 == 4));
+			LuaState l = new LuaState();
+
+			TextAsset luaText = luaAsset.Load("04_ScriptsFromFile") as TextAsset;
+			if(luaText != null)
+				l.DoString(luaText.text);
+
+			TextAsset luaText1 = luaAsset.Load("041_ScriptsFromFile") as TextAsset;
+			if(luaText1 != null)
+				l.DoString(luaText1.text);
+
+		}));
+	}
+
+	public IEnumerator AsyncScript (string luaFileName, System.Action<AssetBundle> onComplete) {
+		string path = "file://" + luaAssetsPath + luaFileName;
+		Debug.LogError ("path: " + path);
+		WWW www = new WWW (path);
+
+		while (!www.isDone)
+			yield return new WaitForEndOfFrame ();
+
+		if (!string.IsNullOrEmpty (www.error))
+			Debug.LogError ("error: " + www.error);
+
+		Debug.LogError (www.assetBundle);
+		onComplete (www.assetBundle);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
